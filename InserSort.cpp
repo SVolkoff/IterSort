68db4648177ab41dbe4eb106c4c5b59c9150dd30
SVolkoff <sabina-volkoff@yandex.com>
2017-04-24 13:51:41 +0300

    Create InserSort.cpp

---
+++ InserSort.cpp
@@ -0,0 +1,10 @@
+template <typename Iterator>
+void insertion_sort(Iterator first, Iterator last)
+{
+	if (!(first < last))
+		return;
+
+	for (Iterator i = first + 1; i != last; ++i)
+		for (Iterator j = i; j != first && *j < *(j - 1); --j)
+			std::iter_swap(j - 1, j);
+}
